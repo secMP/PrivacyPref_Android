d7b9c071dacaaa74bc2c3e69e20087c9367c2653
secMP <prm73@pitt.edu>
2023-11-09 08:02:51 -0500

    Adding the Notification Icon

---
+++ app/src/main/java/com/example/privacypref_android/MyFirebaseMessagingService.kt
@@ -0,0 +1,13 @@
+package com.example.privacypref_android
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    // Create Notification
+
+    // Attach the Notification Custom Layout
+
+    // Display the notification
+
+}
